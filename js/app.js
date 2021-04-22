da8857cf03b5aec5c3fb3b25c37096ca1dc8c1b7
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-04-22 14:59:26 +0200

    [ADD] - Virtual Tour Page

--- js/app.js
+++ js/app.js
@@ -1,27 +1,33 @@
 /* #Component Video Slider Slider
 ================================================== */
-var swiper = new Swiper('.component-video-slider .swiper-container', {
-  slidesPerView: 3.65,
-  breakpoints: {
-    0: {
-      slidesPerView: 1.5
-    },
-    1024: {
-      slidesPerView: 3.65,
+if (document.querySelector('.component-video-slider')) {
+  var swiper = new Swiper('.component-video-slider .swiper-container', {
+    slidesPerView: 3.65,
+    breakpoints: {
+      0: {
+        slidesPerView: 1.5
+      },
+      1024: {
+        slidesPerView: 3.65,
+      }
     }
-  }
-});
+  });
+}
 
 /* #Component Years - Flipdown JS
 ================================================== */
-new FlipDown(1593561600, "flipdown").start();
+if (document.querySelector('.flipdown')) {
+  new FlipDown(1593561600, "flipdown").start();
+}
 
 /* #Home Virtual Tour
 ================================================== */
-var homeVirtualTour = new Vue({
-  el: '#homeVirtualTour',
-  data: {
-    zero: false,
-    classic: false
-  }
-})
+if (document.querySelector('#homeVirtualTour')) {
+  var homeVirtualTour = new Vue({
+    el: '#homeVirtualTour',
+    data: {
+      zero: false,
+      classic: false
+    }
+  });
+}
